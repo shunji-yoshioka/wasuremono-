7ba8ed9758b3f23da55f147db706874e388f9474
吉岡 俊二 <shunji@yoshiokanoMacBook-Pro.local>
2025-04-23 04:14:33 +0900

    初回コミット：ソースコードのみ

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false,
+      enableRemoteModule: true
+    },
+    resizable: true,
+    fullscreenable: true
+  });
+
+  win.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+  // win.webContents.openDevTools(); // コメントアウト
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+}); 
